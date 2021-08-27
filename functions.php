5a7736c08ec92783913ce9641c3bb1c978909e77
Enrique de Larrazabal <enrique.de.larrazabal@gmail.com>
2021-08-27 22:35:22 +0800

    added some shit

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+	$version = wp_get_theme()->get("Version");
+
 	function enrique_theme_support() {
 		// Add title tag support
 		add_theme_support("title-tag");
@@ -16,5 +18,15 @@
 	function enrique_register_styles() {
 		wp_enqueue_style("enrique-master-style", get_template_directory_uri() . "/style.css", array(), "1.0", "all");
 	}
+
+	add_action("wp_enqueue_scripts", "enrique_register_styles");
+
+
+	function enrique_register_scripts() {
+		wp_enqueue_script("enrique-jquery", "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js", array(), "3.6.0", true);
+		wp_enqueue_script("enrique-master-js", get_template_directory_uri() . "/assets/js/main.js", array(), "1.0",true);
+	}
+
+	add_action("wp_enqueue_scripts", "enrique_register_scripts");
 
 ?>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width">
+
+		<?php
+			wp_head();
+		?>
+
+	</head>
+	<body>
